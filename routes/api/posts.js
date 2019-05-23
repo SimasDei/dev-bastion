3076c5879c283e5e0a7872c6c43fffb14f795b8f
Simas <simonas@digitouch.lt>
2019-05-23 16:37:05 +0300

    add Comment Post/Delete routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -59,7 +59,7 @@ router.post(
   [
     auth,
     [
-      check('text', 'Comment cannot be empty')
+      check('text', 'Post cannot be empty')
         .not()
         .isEmpty(),
     ],
@@ -67,7 +67,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     try {
@@ -164,6 +164,81 @@ router.put('/unlike/:id', auth, async (req, res) => {
     await post.save();
 
     res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route - POST api/posts/comment/:id
+ * @description - Comment on a Post
+ * @access - Private
+ */
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Comment cannot be empty')
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  },
+);
+
+/**
+ * @route - DELETE api/posts/comment/:id/:comment_id
+ * @description - Delete comment from Post
+ * @access - Private
+ */
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'No such post' });
+    }
+
+    const comment = post.comments.find(comment => (comment.id = req.params.comment_id));
+    if (!comment) {
+      return res.status(404).json({ msg: 'No such comment' });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not Authorized' });
+    }
+
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server Error');
