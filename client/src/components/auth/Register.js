622f673f84a92332da947ea54ec27c7e159d77b9
Simas <simonas@digitouch.lt>
2019-05-24 09:29:41 +0300

    setup redux, add alert reducer and actions

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,7 +20,7 @@ const Register = () => {
     e.preventDefault();
 
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
       return;
     } else {
       console.log('Great Success !');
@@ -82,4 +85,11 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(
+  null,
+  { setAlert },
+)(Register);
---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Alert = ({ alerts }) =>
+  alerts !== null &&
+  alerts.length > 0 &&
+  alerts.map(alert => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {alert.msg}
+    </div>
+  ));
+
+const mapStateToProps = state => ({
+  alerts: state.alert,
+});
+
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(Alert);
