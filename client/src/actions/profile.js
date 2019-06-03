e90c082bb67987fd0fd6bafd7f29ab78635c6330
Simas <simonas@digitouch.lt>
2019-06-03 08:47:05 +0300

    add profile create functionality

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// import { setAlert } from './alert';
+import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 export const getCurrentProfile = () => async dispatch => {
@@ -11,6 +11,35 @@ export const getCurrentProfile = () => async dispatch => {
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: error.response.statusText, status: error.response.status },
+    });
+  }
+};
+
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+    dispatch({ type: GET_PROFILE, payload: res.data });
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
     });
   }
 };
--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -1,8 +1,10 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -35,7 +37,12 @@ const CreateProfile = props => {
     instagram,
   } = formData;
 
-  const onChange = e => setFormData({ ...formData, [e.target.name]: [e.taget.value] });
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
 
   return (
     <Fragment>
@@ -44,7 +51,7 @@ const CreateProfile = props => {
         <i className="fas fa-user" /> Let's get some information to make your profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -122,18 +129,16 @@ const CreateProfile = props => {
           />
           <small className="form-text">Tell us a little about yourself</small>
         </div>
-
         <div className="my-2">
           <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
             type="button"
             className="btn btn-light"
-            onClick={() => toggleSocialInputs(!displaySocialInputs)}
           >
             Add Social Network Links
           </button>
           <span>Optional</span>
         </div>
-
         {displaySocialInputs && (
           <Fragment>
             <div className="form-group social-input">
@@ -146,7 +151,6 @@ const CreateProfile = props => {
                 onChange={e => onChange(e)}
               />
             </div>
-
             <div className="form-group social-input">
               <i className="fab fa-facebook fa-2x" />
               <input
@@ -157,7 +161,6 @@ const CreateProfile = props => {
                 onChange={e => onChange(e)}
               />
             </div>
-
             <div className="form-group social-input">
               <i className="fab fa-youtube fa-2x" />
               <input
@@ -168,7 +171,6 @@ const CreateProfile = props => {
                 onChange={e => onChange(e)}
               />
             </div>
-
             <div className="form-group social-input">
               <i className="fab fa-linkedin fa-2x" />
               <input
@@ -179,7 +181,6 @@ const CreateProfile = props => {
                 onChange={e => onChange(e)}
               />
             </div>
-
             <div className="form-group social-input">
               <i className="fab fa-instagram fa-2x" />
               <input
@@ -192,16 +193,18 @@ const CreateProfile = props => {
             </div>
           </Fragment>
         )}
-
         <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">
+        <Link className="btn btn-light my-1" to="/dashboard">
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
 };
-
-CreateProfile.propTypes = {};
-
-export default CreateProfile;
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
+export default connect(
+  null,
+  { createProfile },
+)(withRouter(CreateProfile));
